efc79f9ed3d8a41476163c2a38d842d9c39fa430
Vitalii Stepanets <vetal8421@gmail.com>
2025-08-15 01:34:44 +0300

    Fourth commit

--- fourth_exercise.py
+++ fourth_exercise.py
@@ -5,23 +5,20 @@ from colorama import Fore, Style
 не відбувається жодної перевірки на помилку 'try, except'
 """
 
+
 def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except IndexError:
-            if func.__name__ == 'get_phone' or func.__name__ == 'remove_number':
-                return Fore.RED + 'Waiting for 1 param([name])' + Style.RESET_ALL
-            elif func.__name__ == 'add_contact' or func.__name__ == 'change_phone':
-                return Fore.RED + 'Waiting for 2 params([name] [phone])' + Style.RESET_ALL
-        except TypeError as exception:
-            return Fore.RED + str(exception) + Style.RESET_ALL
+            return Fore.RED + 'Need more params' + Style.RESET_ALL
         except ValueError:
-            return Fore.RED + 'No phone numbers found' + Style.RESET_ALL
+            return Fore.RED + "Input 'str' for [name] and 'int' for [phone]" + Style.RESET_ALL
         except KeyError:
-            return Fore.RED + 'No contacts with that name.' + Style.RESET_ALL
+            return Fore.RED + 'No such contact in list' + Style.RESET_ALL
     return inner
 
+@input_error
 def user_input(mes_to_user):
     first_param, *args = input(mes_to_user).split()
     first_param = first_param.lower()
@@ -33,43 +30,32 @@ def greeting():
 
 @input_error
 def get_phone(phone_numbers, *args):
-    if len(args) != 1:
-        raise IndexError
-    else:
         return f"{args[0]} number: {phone_numbers[args[0]]}"
 
 @input_error
 def get_all_phones(phone_numbers, *args):
     if phone_numbers:
         return [x for x in phone_numbers.items()]
+    elif args:
+        return Fore.RED + 'Do not neet params' + Style.RESET_ALL
     else:
-        raise ValueError
+        return Fore.RED + 'No phone numbers' + Style.RESET_ALL
 
 @input_error
 def add_contact(phone_numbers, *args):
-    if not args[0].isalpha():
-        raise TypeError('Only alpha characters are allowed in [name].')
-    elif not args[1].isdigit():
-        raise TypeError('Only digit numbers are allowed in [phone].')
-    elif (len(args)) > 2:
-        raise IndexError
+    if (len(args)) > 2:
+        return Fore.RED + "Waiting for 2 params. Instead got more." + Style.RESET_ALL
     else:
         phone_numbers[args[0]] = int(args[1])
         return Fore.YELLOW + 'Contact added.' + Style.RESET_ALL
 
 @input_error
 def change_phone(phone_numbers, *args):
-    if not args[0].isalpha():
-        raise TypeError('Only alpha characters are allowed in [name].')
-    elif args[0] not in phone_numbers:
-        raise KeyError
-    elif not args[1].isdigit():
-        raise TypeError('Only digit numbers are allowed in [phone].')
-    elif (len(args)) > 2:
-        raise IndexError
+    if (len(args)) > 2:
+        return Fore.RED + "Waiting for 2 params. Instead got more." + Style.RESET_ALL
     else:
         phone_numbers[args[0]] = int(args[1])
-        return Fore.YELLOW + 'Contact changed.' + Style.RESET_ALL
+        return Fore.YELLOW + 'Contact changed(Or created new one).' + Style.RESET_ALL
 
 @input_error
 def remove_number(phone_numbers, *args):
@@ -106,11 +92,7 @@ def main():
             case 'remove':
                 print(remove_number(phone_numbers, *args))
             case 'all':
-                number_list = get_all_phones(phone_numbers, *args)
-                if type(number_list) == str:
-                    print(number_list)
-                elif type(number_list) == list:
-                    print(*number_list, sep='\n')
+                print(get_all_phones(phone_numbers))
             case 'close' | 'exit' | 'quit':
                 print(Fore.YELLOW + 'Goodbye!' + Style.RESET_ALL)
                 break
